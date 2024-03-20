df23ead386d8227e424c70c147c7f27a86fb9c86
tthehuman <miket5279@gmail.com>
2024-03-19 20:11:16 -0700

    Figuring out problems, using html default files; the voices are getting louder

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,26 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-string htmlTag = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WireFrame</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <h1>Planck</h1> 
-    <h3>An open-source, customizable app for developers to communicate and make changes seamlessly.</h3>
-    <br>
-    <button type="button">Do Shit IDK</button> 
-    <hr>
-    <h5>MIT License | Copyright (c) 2024 TTheHuman and 5DHD</h5>
-</body>
-</html>
-""";
-
-app.MapGet("/", () => Results.Content(htmlTag, "text/html"));
+app.UseDefaultFiles();
 
 app.Run();
