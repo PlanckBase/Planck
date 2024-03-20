a31d331f61f9bfc93aeba881ec559faea0ea8b69
tthehuman <miket5279@gmail.com>
2024-03-19 19:38:31 -0700

    did thing !!!!!

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+string htmlTag = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WireFrane</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Planck</h1> 
+    <h3>An open-source, customizable app for developers to communicate and make changes seamlessly.</h3>
+    <hr>
+    <h5>MIT License | Copyright (c) 2024 TTheHuman and 5DHD</h5>
+</body>
+</html>
+""";
+
+app.MapGet("/", () => Results.Content(htmlTag, "text/html"));
 
 app.Run();
